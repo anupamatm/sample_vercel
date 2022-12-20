63c708c2aaf4ebd21aa8c709464d153857cc20f7
anupama <anupamaunni612@gmail.com>
2022-12-20 15:34:18 +0530

    static bash collect

---
+++ staticfiles_build/static/js/even.js
@@ -0,0 +1,13 @@
+num=Number(prompt('enter anumber'))
+function even(num)
+{
+    if(num%2==0)
+    {
+        console.log('Even number')
+        document.write('even number')
+    }
+    else{
+        console.log('odd number')
+        document.write('odd number')
+    }
+}
